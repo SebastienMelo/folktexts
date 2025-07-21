6a00a59ebf90dff634cbca361f751a341cb63583
SebastienMelo <sebastien.melo@polytechnique.edu>
2025-07-21 15:13:39 +0200

    batch size increase

--- JZay_exec.sh
+++ JZay_exec.sh
@@ -12,7 +12,7 @@
 #SBATCH -A nuj@h100
 #SBATCH -C h100
 #SBATCH --hint=nomultithread
-#SBATCH --time=20:00:00
+#SBATCH --time=:00:00
 
 #SBATCH --array=0-7
 
@@ -33,11 +33,11 @@ module load pytorch-gpu/py3/2.7.0
 
 model_dir=$DSDIR/HuggingFace_Models/
 
-A_VALUES=('meta-llama/Llama-3-70B-Instruct' 'Qwen/Qwen2.5-VL-7B-Instruct'
+A_VALUES=('meta-llama/Meta-Llama-3-70B-Instruct' 'Qwen/Qwen2.5-VL-7B-Instruct'
          'Qwen/Qwen2.5-VL-32B-Instruct' 'Qwen/Qwen2.5-72B-Instruct'
          'google/gemma-2b-it' 'google/gemma-3-4b-it'
          'google/gemma-2-9b-it' 'google/gemma-3-27b-it')
 
 
-python -m folktexts.cli.run_acs_benchmark --model $model_dir${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task ACSIncome --data-dir data --results-dir folktexts-results
+python -m folktexts.cli.run_acs_benchmark --model $model_dir${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task ACSIncome --data-dir data --results-dir folktexts-results --batch-size 32
 # srun run_acs_benchmark --model ${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task ACSIncome --results-dir folktexts-results --data-dir data
