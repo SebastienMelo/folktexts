da39407e7537fd26047f6c27cd84d011367be79d
SebastienMelo <sebastien.melo@polytechnique.edu>
2025-07-23 15:11:46 +0200

    qwen

--- JZay_exec.sh
+++ JZay_exec.sh
@@ -34,7 +34,7 @@ module load pytorch-gpu/py3/2.7.0
 
 model_dir=$DSDIR/HuggingFace_Models/
 
-A_VALUES=('Qwen/Qwen2.5-VL-32B-Instruct' 'meta-llama/Meta-Llama-3-8B')
+A_VALUES=('Qwen/Qwen2.5-32B-Instruct' 'Qwen/Qwen2.5-72B-Instruct')
 
 
 python -m folktexts.cli.run_acs_benchmark --model $model_dir${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task ACSIncome --data-dir data --results-dir folktexts-results --batch-size 64
