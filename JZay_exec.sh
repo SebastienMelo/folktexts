eaac7d4411d4faa88bb531254634c8d9919120fd
SebastienMelo <sebastien.melo@polytechnique.edu>
2025-08-29 21:18:51 +0200

    numeric prompting

--- JZay_exec.sh
+++ JZay_exec.sh
@@ -12,10 +12,10 @@
 #SBATCH -A nuj@h100
 #SBATCH -C h100
 #SBATCH --hint=nomultithread
-#SBATCH --time=70:00:00
+#SBATCH --time=72:00:00
 #SBATCH --qos=qos_gpu_h100-t4
 
-#SBATCH --array=0-18
+#SBATCH --array=0-26
 
 echo "------------------------------------------------"
 echo "Slurm Job ID: $SLURM_JOB_ID"  
@@ -34,12 +34,15 @@ module load pytorch-gpu/py3/2.7.0
 
 model_dir=$DSDIR/HuggingFace_Models/
 
-A_VALUES=('01-ai/Yi-1.5-34B-Chat' '01-ai/Yi-1.5-34B' '01-ai/Yi-1.5-9B-Chat' '01-ai/Yi-1.5-9B' 
-'google/gemma-2-9b' 'google/gemma-2-27b' 'google/gemma-2-27b-it'
-'mistralai/Mistral-Small-3.2-24B-Instruct-2506' 'mistralai/Mistral-Small-3.1-24B-Base-2503' 'mistralai/Mistral-7B-Instruct-v0.3' 'mistralai/Mistral-7B-v0.3'
-'meta-llama/Llama-2-13b-chat-hf' 'meta-llama/Llama-3.2-1B-Instruct' 'meta-llama/Llama-3.2-3B' 'meta-llama/Llama-3.2-3B-Instruct' 'meta-llama/Llama-Guard-3-1B' 'meta-llama/Llama-3.1-8B' 'meta-llama/Llama-Guard-3-8B'
-'microsoft/Orca-2-7b')
+A_VALUES=('google/gemma-2-9b' 'google/gemma-2-27b' 'google/gemma-2-27b-it' 'google/gemma-2-9b-it'
+'mistralai/Mistral-7B-Instruct-v0.3' 'mistralai/Mistral-7B-v0.3' 'mistralai/Mixtral-8x7B-Instruct-v0.1' 'mistralai/Mixtral-8x7B-v0.1'
+'meta-llama/Llama-2-13b-chat-hf' 'meta-llama/Llama-2-13b-hf' 'meta-llama/Llama-2-70b-chat-hf' 'meta-llama/Llama-2-70b-hf'
+'meta-llama/Llama-3.1-8B' 'meta-llama/Llama-3.1-8B-Instruct' 'meta-llama/Llama-3.2-1B' 'meta-llama/Llama-3.2-1B-Instruct'
+'meta-llama/Llama-3.2-3B' 'meta-llama/Llama-3.2-3B-Instruct' 'meta-llama/Llama-3.3-70B-Instruct' 'meta-llama/Meta-Llama-3-70B'
+'meta-llama/Meta-Llama-3-70B-Instruct' 'meta-llama/Meta-Llama-3-8B' 'meta-llama/Meta-Llama-3-8B-Instruct'
+'microsoft/Orca-2-13b' 'microsoft/phi-4'
+'deepseekai/DeepSeek-R1-Distill-Llama-8B' 'deepseekai/DeepSeek-R1-Distill-Llama-70B')
 
 
-python -m folktexts.cli.run_acs_benchmark --model $model_dir${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task ACSIncome --data-dir data --results-dir folktexts-results --batch-size 32
+python -m folktexts.cli.run_acs_benchmark --model $model_dir${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task ACSIncome --data-dir data --results-dir folktexts-results-numeric_prompt --batch-size 32 --numeric-risk-prompting True
 # srun run_acs_benchmark --model ${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task ACSIncome --results-dir folktexts-results --data-dir data
