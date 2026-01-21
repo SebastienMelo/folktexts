67e7354ff685516242c14695f3894718b654b30e
SebastienMelo <sebastien.melo@polytechnique.edu>
2026-01-21 15:06:18 +0100

    acsemployment deepseek correct path

--- JZay_exec.sh
+++ JZay_exec.sh
@@ -34,10 +34,9 @@ module load pytorch-gpu/py3/2.7.0
 
 model_dir=$DSDIR/HuggingFace_Models/
 
-A_VALUES=('microsoft/Orca-2-13b' 'microsoft/phi-4'
-'deepseek-ai/DeepSeek-R1-Distill-Llama-8B' 'deepseek-ai/DeepSeek-R1-Distill-Llama-70B')
+A_VALUES=('deepseek-ai/DeepSeek-R1-Distill-Llama-8B' 'deepseek-ai/DeepSeek-R1-Distill-Llama-70B')
 
-T_VALUES=('ACSIncome' 'ACSMobility' 'ACSTravelTime' 'ACSPublicCoverage')
+T_VALUES=('ACSEmployment')
 
 
 NUM_MODELS=${#A_VALUES[@]}
@@ -46,5 +45,5 @@ NUM_TASKS=${#T_VALUES[@]}
 MODEL_IDX=$((SLURM_ARRAY_TASK_ID / NUM_TASKS))
 TASK_IDX=$((SLURM_ARRAY_TASK_ID % NUM_TASKS))
 
-python -m folktexts.cli.run_acs_benchmark --model "${model_dir}${A_VALUES[$MODEL_IDX]}" --task "${T_VALUES[$TASK_IDX]}" --data-dir data --results-dir folktexts-results-ICML --batch-size 16 
+python -m folktexts.cli.run_acs_benchmark --model "${model_dir}${A_VALUES[$MODEL_IDX]}" --task "${T_VALUES[$TASK_IDX]}" --data-dir data --results-dir folktexts-results-ICML --batch-size 16 --subsampling 0.5 --seed 42
 # srun run_acs_benchmark --model ${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task ACSIncome --results-dir folktexts-results --data-dir data
