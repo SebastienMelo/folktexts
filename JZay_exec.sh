5eb5c80da513c21d6ae36f4737812f2b84c8aa7a
SebastienMelo <sebastien.melo@polytechnique.edu>
2026-01-21 11:41:09 +0100

    final icml

--- JZay_exec.sh
+++ JZay_exec.sh
@@ -15,7 +15,7 @@
 #SBATCH --time=90:00:00
 #SBATCH --qos=qos_gpu_h100-t4
 
-#SBATCH --array=0-26
+#SBATCH --array=0-15
 
 echo "------------------------------------------------"
 echo "Slurm Job ID: $SLURM_JOB_ID"  
@@ -34,14 +34,17 @@ module load pytorch-gpu/py3/2.7.0
 
 model_dir=$DSDIR/HuggingFace_Models/
 
-A_VALUES=('google/gemma-2-9b' 'google/gemma-2-27b' 'google/gemma-2-27b-it' 'google/gemma-2-9b-it'
-'mistralai/Mistral-7B-Instruct-v0.3' 'mistralai/Mistral-7B-v0.3' 'mistralai/Mixtral-8x7B-Instruct-v0.1' 'mistralai/Mixtral-8x7B-v0.1'
-'meta-llama/Llama-2-13b-chat-hf' 'meta-llama/Llama-2-13b-hf' 'meta-llama/Llama-2-70b-chat-hf' 'meta-llama/Llama-2-70b-hf'
-'meta-llama/Llama-3.1-8B' 'meta-llama/Llama-3.1-8B-Instruct' 'meta-llama/Llama-3.2-1B' 'meta-llama/Llama-3.2-1B-Instruct'
-'meta-llama/Llama-3.2-3B' 'meta-llama/Llama-3.2-3B-Instruct' 'meta-llama/Llama-3.3-70B-Instruct' 'meta-llama/Meta-Llama-3-70B'
-'meta-llama/Meta-Llama-3-70B-Instruct' 'meta-llama/Meta-Llama-3-8B' 'meta-llama/Meta-Llama-3-8B-Instruct'
-'microsoft/Orca-2-13b' 'microsoft/phi-4'
-'deepseekai/DeepSeek-R1-Distill-Llama-8B' 'deepseekai/DeepSeek-R1-Distill-Llama-70B')
+A_VALUES=('microsoft/Orca-2-13b' 'microsoft/phi-4'
+'deepseek-ai/DeepSeek-R1-Distill-Llama-8B' 'deepseek-ai/DeepSeek-R1-Distill-Llama-70B')
 
-python -m folktexts.cli.run_acs_benchmark --model $model_dir${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task 'ACSEmployment' --data-dir data --results-dir folktexts-results-ICML --batch-size 16 --subsampling 0.5 --seed 42
+T_VALUES=('ACSIncome' 'ACSMobility' 'ACSTravelTime' 'ACSPublicCoverage')
+
+
+NUM_MODELS=${#A_VALUES[@]}
+NUM_TASKS=${#T_VALUES[@]}
+
+MODEL_IDX=$((SLURM_ARRAY_TASK_ID / NUM_TASKS))
+TASK_IDX=$((SLURM_ARRAY_TASK_ID % NUM_TASKS))
+
+python -m folktexts.cli.run_acs_benchmark --model "${model_dir}${A_VALUES[$MODEL_IDX]}" --task "${T_VALUES[$TASK_IDX]}" --data-dir data --results-dir folktexts-results-ICML --batch-size 16 
 # srun run_acs_benchmark --model ${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task ACSIncome --results-dir folktexts-results --data-dir data
