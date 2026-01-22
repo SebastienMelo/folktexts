0730fa5a332796de72cfb99baf04d0aaaa444727
SebastienMelo <sebastien.melo@polytechnique.edu>
2026-01-22 16:54:49 +0100

    few samples Llama 70B

--- JZay_exec.sh
+++ JZay_exec.sh
@@ -36,7 +36,7 @@ model_dir=$DSDIR/HuggingFace_Models/meta-llama/Llama-3.3-70B-Instruct
 
 # A_VALUES=('google/gemma-3-12b-pt' 'google/gemma-3-12b-it' 'google/gemma-3-27b-pt' 'google/gemma-3-27b-it')
 T_VALUES=('ACSTravelTime' 'ACSIncome' 'ACSPublicCoverage' 'ACSMobility' 'ACSEmployment')
-S_VALUES=(0.6 0.6 0.6 0.6 0.3) 
+S_VALUES=(0.4 0.4 0.4 0.4 0.2) 
 
 python -m folktexts.cli.run_acs_benchmark --model $model_dir --task ${T_VALUES[$SLURM_ARRAY_TASK_ID]} --data-dir data --results-dir folktexts-results-metamodel-Llama70B --batch-size 8  --subsampling ${S_VALUES[$SLURM_ARRAY_TASK_ID]}
 # srun run_acs_benchmark --model ${A_VALUES[$SLURM_ARRAY_TASK_ID]} --task ACSIncome --results-dir folktexts-results --data-dir data
